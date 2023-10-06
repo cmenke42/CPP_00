c2b5bc0abed127363115e570e7be7d1bf4e990ef
Carlos Menke <cmenke@c2s19d122.42wolfsburg.de>
2023-10-06 22:47:59 +0200

    modified the tests

--- ex00/test/test.sh
+++ ex00/test/test.sh
@@ -1,51 +1,64 @@
 #!/bin/bash
 
-# Define ANSI color
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 RED='\033[0;31m'
 RESET='\033[0m'
 
-# Determine the script's directory
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+MEGAPHONE_EXECUTABLE="../megaphone"
 
-# Set the path to the "megaphone" executable
-MEGAPHONE_EXECUTABLE="$SCRIPT_DIR/../megaphone"
+file1="expected.txt"
+file2="output.txt"
 
-file1="$SCRIPT_DIR/expected.txt"
-file2="$SCRIPT_DIR/output.txt"
-
-# Open test
-exec > >(tee "$file2")
-
-# Run tests
-"$MEGAPHONE_EXECUTABLE" "shhhhh... I think the students are asleep..."
-"$MEGAPHONE_EXECUTABLE" Damnit " ! " "Sorry students, I thought this thing was off."
-"$MEGAPHONE_EXECUTABLE"
-
-# Close test
-exec >/dev/tty
+# Create a subshell
+(
+    #Output to terminal and file
+    exec > >(tee "$file2")
+    "$MEGAPHONE_EXECUTABLE" "shhhhh... I think the students are asleep..." | cat -e
+    "$MEGAPHONE_EXECUTABLE" Damnit " ! " "Sorry students, I thought this thing was off." | cat -e
+    "$MEGAPHONE_EXECUTABLE" | cat -e
+)
 
 # Compare results
 echo -e "\n${YELLOW}Comparing output with expected:${RESET}"
 if diff "$file1" "$file2" >/dev/null; then
     echo -e "${GREEN}Success${RESET}"
 else
-    {
-        echo -e "${RED}Fail${RESET}"
-        awk -v yellow="$YELLOW" -v green="$GREEN" -v red="$RED" -v reset="$RESET" '
-            NR==FNR {
-                a[NR]=$0
-                next
-            } {
-                if (a[FNR] != $0) {
+{
+    echo -e "${RED}Fail${RESET}"
+    awk -v yellow="$YELLOW" -v green="$GREEN" -v red="$RED" -v reset="$RESET" '
+        {
+            if (FILENAME == ARGV[1]) {
+                # Process the first file to store expected lines
+                expected_lines[FNR] = $0
+                length_file_1 = FNR
+            } else {
+                # Process the second file to compare expected and actual lines
+                if (FNR <= length_file_1 && expected_lines[FNR] != $0) {
                     print yellow "Difference at line", reset, FNR
-                    print green "Correct:", reset, a[FNR]
-                    print red "Wrong  :", reset, $0
+                    print green "Expected:", reset, expected_lines[FNR]
+                    print red "Actual  :", reset, $0
+                } else if ((FNR == length_file_1 && expected_lines[FNR] != $0) || FNR > length_file_1) {
+                    # Print extra lines in the output
+                    print yellow "Difference at line", reset, FNR
+                    print green "Expected:", reset
+                    print red "Actual  :", reset, $0
                 }
+                length_file_2 = FNR  # Store the length of file 2
             }
+        }
+        END {
+                if (length_file_2 < length_file_1) {
+                    #Print lines that are missing in the output
+                    for (i = length_file_2 + 1; i <= length_file_1; i++) {
+                        print yellow "Difference at line", reset, i
+                        print green "Expected:", reset, expected_lines[i]
+                        print red "Actual  :", reset
+                    }
+            }
+        }
         ' "$file1" "$file2"
-    }
+}
 fi
 
 # Clean up
--- ex02/test/test.sh
+++ ex02/test/test.sh
@@ -12,17 +12,16 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 # Set the path to the executable
 EXECUTABLE="$SCRIPT_DIR/../bank_account"
 
-
+    
 file1="$SCRIPT_DIR/expected.log"
 file2="$SCRIPT_DIR/output.txt"
 file1_timestamp_removed="$SCRIPT_DIR/expected_timestamp_removed.txt"
 file2_timestamp_removed="$SCRIPT_DIR/output_timestamp_removed.txt"
-file3_test_result="$SCRIPT_DIR/test_result.txt"
 
 # Start a subshell
 (
     exec 2>&1 > >(tee "$file2")
-    "$EXECUTABLE"
+    "$EXECUTABLE" | cat -e
 )
 # Back to the main shell
 
@@ -35,22 +34,41 @@ echo -e "\n${YELLOW}Comparing output with expected:${RESET}"
 if diff "$file1_timestamp_removed" "$file2_timestamp_removed" >/dev/null; then
     echo -e "${GREEN}Success${RESET}"
 else
-    {
-        echo -e "${RED}Fail${RESET}"
-        awk -v yellow="$YELLOW" -v green="$GREEN" -v red="$RED" -v reset="$RESET" '
-            NR==FNR {
-                a[NR]=$0
-                next
-            } {
-                if (a[FNR] != $0) {
+{
+    echo -e "${RED}Fail${RESET}"
+    awk -v yellow="$YELLOW" -v green="$GREEN" -v red="$RED" -v reset="$RESET" '
+        {
+            if (FILENAME == ARGV[1]) {
+                # Process the first file to store expected lines
+                expected_lines[FNR] = $0
+                length_file_1 = FNR
+            } else {
+                # Process the second file to compare expected and actual lines
+                if (FNR <= length_file_1 && expected_lines[FNR] != $0) {
                     print yellow "Difference at line", reset, FNR
-                    print green "Correct:", reset, a[FNR]
-                    print red "Wrong  :", reset, $0
+                    print green "Expected:", reset, expected_lines[FNR]
+                    print red "Actual  :", reset, $0
+                } else if ((FNR == length_file_1 && expected_lines[FNR] != $0) || FNR > length_file_1) {
+                    # Print extra lines in the output
+                    print yellow "Difference at line", reset, FNR
+                    print green "Expected:", reset
+                    print red "Actual  :", reset, $0
                 }
+                length_file_2 = FNR  # Store the length of file 2
             }
-        ' "$file1_timestamp_removed" "$file2_timestamp_removed" > "$file3_test_result"
-        cat "$file3_test_result"
-    }
+        }
+        END {
+                if (length_file_2 < length_file_1) {
+                    #Print lines that are missing in the output
+                    for (i = length_file_2 + 1; i <= length_file_1; i++) {
+                        print yellow "Difference at line", reset, i
+                        print green "Expected:", reset, expected_lines[i]
+                        print red "Actual  :", reset
+                    }
+            }
+        }
+        ' "$file1_timestamp_removed" "$file2_timestamp_removed"
+}
 fi
 
 # Clean up
